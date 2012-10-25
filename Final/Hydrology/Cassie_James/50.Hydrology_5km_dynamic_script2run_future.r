f6bc0e5969880629319a6154c6ffa835d4b91acd
Cassandra James <cassiesjames@jcu.edu.au>
2012-10-25 10:49:53 +1000

    Hydrology for the futures static script

--- Final/Hydrology/Cassie_James/50.Hydrology_5km_dynamic_script2run_future.r
+++ Final/Hydrology/Cassie_James/50.Hydrology_5km_dynamic_script2run_future.r
@@ -9,7 +9,8 @@ out.dir='/home/jc246980/Hydrology.trials/Outputs/Outputs_Futures/'
 future.dir="/home/jc165798/Climate/CIAS/Australia/5km/monthly_csv/"
 
 ESs=list.files(future.dir, pattern='RCP')
-GCMs = list.files(paste(futures.dir,pattern=ESs[1],sep=''))
+GCMs = list.files(paste(future.dir,pattern=ESs[1],sep=''))
+YEARs=seq(2015, 2085,10)
 
 #### read in inputs
 base.asc='/home/jc165798/Climate/AWAP.direct.download/summaries/Oz/base.asc.gz'
@@ -23,43 +24,53 @@ pos$kRs[which(is.na(pos$kRs))] = 0.16 #set missing data to 0.16
 pos$DEM = extract.data(cbind(pos$lon,pos$lat),read.asc('inputs/DEM_5km.asc'))
 
 #### load Budyko code 
-dyn.load('/home/jc246980/Scripts/Final/Hydrology/Cassie_James/05.Budyko.so') #load the C code
+dyn.load('/home/jc246980/Freshwater_scripts/Final/Hydrology/Cassie_James/05.Budyko.so') #load the C code
 
-#### Run Budyko code -- dynamic analysis
+#### Run Budyko code -- staticanalysis
 
 	for(es in ESs) { 
 	
 		for(gcm in GCMs) {
 			
-			tmin = read.csv(paste(future.dir,es,"/",gcm,"/","tmn.matrix.csv",sep=''),as.is=TRUE) #read in monthly tmin
-			tmax = read.csv(paste(future.dir,es,"/",gcm,"/","tmx.matrix.csv",sep=''),as.is=TRUE) #read in monthly tmax
-			pr = read.csv(paste(future.dir,es,"/",gcm,"/","pre.matrix.csv",sep=''),as.is=TRUE) #read in precipitatation
-
-			tmin = tmin[,c(-1,-2)] # remove longs and lats from the start of the data file
-			tmax= tmax[,c(-1,-2)] # remove longs and lats from the start of the data file
-			tmin = tmax[,c(-1,-2)] # remove longs and lats from the start of the data file
-
-			###run the analysis and write out data
-			tt = .Call('BudykoBucketModelDynamic',
-				pos$DEM, #dem info
-				as.matrix(pr), # monthly precip
-				as.matrix(tmin), #monthly tmin
-				as.matrix(tmax), #monthly tmax
-				pos$PAWHC, #soil water holding capacity
-				pos$kRs, #unknown kRs values
-				pos$lat / (180/pi), #latitudes in radians
-				nrow(pr), #number of rows that need run
-				ncol(pr) #number of months to run through
-			)
-		
-			setwd(out.dir)
-
-			Eact = tt[[1]]; save(Eact, file=paste(out.dir,"Eact/",es,"_",gcm,".Rdata",sep='')) #save the actual evapotranspiration
-			Epot = tt[[2]]; save(Epot, file=paste(out.dir,"Epot/",es,"_",gcm,".Rdata",sep='')) #save the potential evapotranspiration
-			Qrun = tt[[3]]; save(Qrun, file=paste(out.dir,"Qrun/",es,"_",gcm,".Rdata",sep='')) #save the runoff
-			Rnet = tt[[4]]; save(Rnet, file=paste(out.dir,"Rnet/",es,"_",gcm,".Rdata",sep='')) #save the net radiation
+				tmin = read.csv(paste(future.dir,es,"/",gcm,"/","tmn.matrix.csv",sep=''),as.is=TRUE) #read in monthly tmin
+				tmax = read.csv(paste(future.dir,es,"/",gcm,"/","tmx.matrix.csv",sep=''),as.is=TRUE) #read in monthly tmax
+				pr = read.csv(paste(future.dir,es,"/",gcm,"/","pre.matrix.csv",sep=''),as.is=TRUE) #read in precipitatation			
 				
+				Eact=matrix(NA,nrow=nrow(tmin),ncol=96)
+				tt = expand.grid(sprintf('%02i',1:12),YEARs=YEARs);tt = paste(tt[,2],tt[,1],sep='_'); colnames(Eact) = tt #add the column names
+				Qrun=Rnet=Epot=Eact
+				
+			for(yy in YEARs) {
+			
+				cois=NULL
+                cois = c(cois,grep(yy,colnames(tmin))); tmin_yoi = tmin[,cois]; tmax_yoi=tmax[,cois]; pr_yoi= pr[,cois] #get the columns in the years of interest
+                
+				###run the analysis and write out data
+				tt = .Call('BudykoBucketModelStatic',
+					pos$DEM, #dem info
+					as.matrix(pr_yoi), # monthly precip
+					as.matrix(tmin_yoi), #monthly tmin
+					as.matrix(tmax_yoi), #monthly tmax
+					pos$PAWHC, #soil water holding capacity
+					pos$kRs, #unknown kRs values
+					pos$lat / (180/pi), #latitudes in radians
+					nrow(pr_yoi), #number of rows that need run
+					ncol(pr_yoi) #number of months to run through
+				)
+			
+			Eact[,grep(yy,colnames(Eact))]=tt[[1]]
+			Epot[,grep(yy,colnames(Epot))]=tt[[2]]
+			Qrun[,grep(yy,colnames(Qrun))]=tt[[3]]
+			Rnet[,grep(yy,colnames(Rnet))]=tt[[4]]
+			
+	
+			}		
 		
+		setwd(out.dir)
+		save(Eact, file=paste(out.dir,"Eact/",es,"_",gcm,".Rdata",sep='')) #save the actual evapotranspiration
+		save(Epot, file=paste(out.dir,"Epot/",es,"_",gcm,".Rdata",sep='')) #save the potential evapotranspiration
+		save(Qrun, file=paste(out.dir,"Qrun/",es,"_",gcm,".Rdata",sep='')) #save the runoff
+		save(Rnet, file=paste(out.dir,"Rnet/",es,"_",gcm,".Rdata",sep='')) #save the net radiation
 		
 		}
 	}
