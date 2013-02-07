f74d288db35178ed885439f6ee007ed61092b224
Cassandra James <cassiesjames@jcu.edu.au>
2013-02-07 12:10:18 +1000

    updates to accumulation

--- Final/Report/Continental_stability_report_accumulatedrunoff_images.r
+++ Final/Report/Continental_stability_report_accumulatedrunoff_images.r
@@ -61,25 +61,20 @@ year=2085
 				deltalabs = c(paste('<',deltalims[1]),deltamid,paste('>',deltalims[2]))
 				
 				for (percentile in c(10,50,90)) { cat(percentile,'\n') #cycle through the percentiles
-					tasc=base.asc; tasc[cbind(pos250$row,pos250$col)]=pos[,paste(es,year,percentile,sep='_')]
-					tasc[which(tasc==0)] = 0
-					tasc[which(tasc>0 & tasc<.25)] = -10
-					tasc[which(tasc>=.25 & tasc<0.5)] = -9
-					tasc[which(tasc>=0.5 & tasc<0.75)] = -8
-					tasc[which(tasc>=0.75 & tasc<1)] = -7
-					tasc[which(tasc>=1 & tasc<2)] = -6
-					tasc[which(tasc>=2 & tasc<5)] = -5
-					tasc[which(tasc>=5 & tasc<10)] = -4
-					tasc[which(tasc>=10 & tasc<50)] = -3
-					tasc[which(tasc>=50 & tasc<100)] = -2
-					#tasc[which(tasc>=100 & tasc<1000)] = -10
-					tasc[which(tasc>=100)] = -1
+					tasc=base.asc; tasc[cbind(pos$row,pos$col)]=pos[,paste(es,year,percentile,sep='_')]
+					tasc[which(tasc>0 & tasc<.25)] = 0
+					tasc[which(tasc>=.25 & tasc<0.5)] = 0.25
+					tasc[which(tasc>=0.5 & tasc<0.75)] = 0.5
+					tasc[which(tasc>=0.75 & tasc<1)] = 0.75
+					tasc[which(tasc>=1 & tasc<2)] = 1
+					tasc[which(tasc>=2 & tasc<5)] = 2
+					tasc[which(tasc>=5)] = 5
 					zlims = range(c(0,as.vector(tasc)),na.rm=TRUE)
 					image(tasc, ann=FALSE,axes=FALSE,col=all.cols,zlim=zlims)
 					plot(Drainageshape , lwd=2, ann=FALSE,axes=FALSE, add=TRUE)
 				}
 
-				  labs=c("<.25",".25",".5",".75","1","2","5","10","50",">100")
+				  labs=c("<0.25","0.5","0.75","1","2",">5")
 				  plot(1:20,axes=FALSE,ann=FALSE,type='n')
 				  text(10,18,"Difference from current",cex=3)
 				  color.legend(2,10,18,15,labs,rect.col=all.cols,align="rb",gradient="x", cex=1.5)
@@ -87,17 +82,4 @@ year=2085
 
 		dev.off() #close out the
 		
-		
-		# zlim=c(min(c(pos$flow,pos$jflow),na.rm=T),max(c(pos$flow,pos$jflow),na.rm=T))
-# # zlim=c(min(c(pos$flow,pos$jflow),na.rm=T),max(tasc,na.rm=T))
-# cols = colorRampPalette(c('tan',"#E0F3F8","#ABD9E9","#90c4de","#74ADD1","#5d91c3","#4575B4","#4067ad","#3c58a6","#37489d","#313695","#191c90",'blue4','#0d0d7d','midnightblue','#131354','#0d0d39','#06071e'))(100)
-
-# pnts=cbind(x=c(146.5,148,148,146.5),y=c(-12,-12,-16,-16))
-
-
-	# image(tasc, ann=F,axes=F,zlim=zlim,col=cols)
-	# mtext('VanDerWal et al 2013', side=1, cex=15)
-	# image(jasc, ann=F,axes=F,zlim=zlim,col=cols)
-
-	# mtext('Stein 2012', side=1,cex=15)
-	# legend.gradient(pnts,col=cols, round(zlim/1000), title='Flow (GL)',cex=12)
+	
